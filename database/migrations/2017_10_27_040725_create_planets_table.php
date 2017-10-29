b877ae37ddc8458e240c22c900c1bf05520660e7
André Ferreira <contato@andreferreira.me>
2017-10-29 19:03:56 -0200

    Update Units columns names

--- database/migrations/2017_10_27_040725_create_planets_table.php
+++ database/migrations/2017_10_27_040725_create_planets_table.php
@@ -22,44 +22,44 @@ class CreatePlanetsTable extends Migration
             // $table->enum('orbitalType', ['foo', 'bar']);
 
             $table->double('mean_radius');
-            $table->integer('mean_radius_unit_id')->unsigned();
-            $table->foreign('mean_radius_unit_id')->references('id')->on('units');
+            $table->integer('mean_radius_unit')->unsigned();
+            $table->foreign('mean_radius_unit')->references('id')->on('units');
 
             $table->double('equatorial_radius');
-            $table->integer('equatorial_radius_unit_id')->unsigned();
-            $table->foreign('equatorial_radius_unit_id')->references('id')->on('units');
+            $table->integer('equatorial_radius_unit')->unsigned();
+            $table->foreign('equatorial_radius_unit')->references('id')->on('units');
 
             $table->double('polar_radius');
-            $table->integer('polar_radius_unit_id')->unsigned();
-            $table->foreign('polar_radius_unit_id')->references('id')->on('units');
+            $table->integer('polar_radius_unit')->unsigned();
+            $table->foreign('polar_radius_unit')->references('id')->on('units');
 
             $table->double('equatorial_circumference');
-            $table->integer('equatorial_circumference_unit_id')->unsigned();
-            $table->foreign('equatorial_circumference_unit_id')->references('id')->on('units');
+            $table->integer('equatorial_circumference_unit')->unsigned();
+            $table->foreign('equatorial_circumference_unit')->references('id')->on('units');
 
             $table->double('meridional_circumference');
-            $table->integer('meridional_circumference_unit_id')->unsigned();
-            $table->foreign('meridional_circumference_unit_id')->references('id')->on('units');
+            $table->integer('meridional_circumference_unit')->unsigned();
+            $table->foreign('meridional_circumference_unit')->references('id')->on('units');
 
             $table->double('mass');
-            $table->integer('mass_unit_id')->unsigned();
-            $table->foreign('mass_unit_id')->references('id')->on('units');
+            $table->integer('mass_unit')->unsigned();
+            $table->foreign('mass_unit')->references('id')->on('units');
 
             $table->double('periapsis');
-            $table->integer('periapsis_unit_id')->unsigned();
-            $table->foreign('periapsis_unit_id')->references('id')->on('units');
+            $table->integer('periapsis_unit')->unsigned();
+            $table->foreign('periapsis_unit')->references('id')->on('units');
 
             $table->double('apoapsis');
-            $table->integer('apoapsis_unit_id')->unsigned();
-            $table->foreign('apoapsis_unit_id')->references('id')->on('units');
+            $table->integer('apoapsis_unit')->unsigned();
+            $table->foreign('apoapsis_unit')->references('id')->on('units');
 
             $table->float('orbital_period');
-            $table->integer('orbital_period_unit_id')->unsigned();
-            $table->foreign('orbital_period_unit_id')->references('id')->on('units');
+            $table->integer('orbital_period_unit')->unsigned();
+            $table->foreign('orbital_period_unit')->references('id')->on('units');
 
             $table->float('rotation_period');
-            $table->integer('rotation_period_unit_id')->unsigned();
-            $table->foreign('rotation_period_unit_id')->references('id')->on('units');
+            $table->integer('rotation_period_unit')->unsigned();
+            $table->foreign('rotation_period_unit')->references('id')->on('units');
 
             $table->boolean('rings');
             $table->text('atmosphere');
